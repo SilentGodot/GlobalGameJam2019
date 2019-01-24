b46b05631c40e21f5bd9546e4679c51f7f05a444
Mor Ohana <Soramora1@Gmail.com>
2019-01-25 00:04:56 +0200

    Initial wave class

---
+++ Assets/Scripts/Wave/Wave.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Wave
+{
+    public class Wave : MonoBehaviour
+    {
+        [SerializeField] protected List<GameObject> _enemyPrefabs;
+
+
+        [SerializeField] protected int _spawnCount;
+        protected int _currentSpawnCount;
+
+        [SerializeField] List<int> _spawnDelays;
+
+        
+        [Space]
+        [Header("Spawn Pos")]
+        protected Transform _spawnPositionTransform;
+        [SerializeField] protected Vector3 _distanceFromPos;
+
+
+        protected bool isSpawning = false;
+
+        protected virtual void Start()
+        {
+            _spawnPositionTransform = transform;
+
+        }
+
+        // Update is called once per frame
+        protected virtual void Update()
+        {
+            _enemyPrefabs = _enemyPrefabs.Where(x => x != null).ToList();
+            Spawn();
+        }
+
+    }
+}
