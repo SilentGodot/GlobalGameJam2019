3beeeab6f93aef75c9e8e3232107017d9055b8d7
Mor Ohana <Soramora1@Gmail.com>
2019-01-26 14:13:43 +0200

    Working menu and scene change

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    static LevelManager _instance;
+
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new LevelManager();
+            return _instance;
+        }
+    }
+
+    // public List<Wave> waves;  
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+  
+    // Use this for initialization
+    void Start () {
+	}
+
+
+
+
+
+
+
+}
